4a3dbf8445e119eabe462c8e7d07089808ccfb39
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2017-02-06 20:42:22 +0000

    Initialized server.py and base.html template

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.secret_key = "BleepBloop"
+
+
+@app.route('/')
+def return_homepage():
+    """Display the app's homepage"""
+
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+
+    DebugToolbarExtension(app)
+
+    app.run(host='0.0.0.0')
