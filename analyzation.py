e663fbcbbaed3b03552c853961f618826dbea962
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2017-02-21 23:32:18 +0000

    Refactored analyzation.py module

--- analyzation.py
+++ analyzation.py
@@ -7,26 +7,27 @@ def get_artist_recs(spotify):
     # Get user's top artists
     top_artists_response = spotify.current_user_top_artists(limit=10,
                                                             time_range='medium_term')
-    top_artists_dict = parse_artist_response(top_artists_response)
+    top_artists_dict = parse_artist_response(top_artists_response['items'])
 
-    # Get artists related to user's top artists
+    # Get artists related to each of the user's top artists
     related_artists_dict = {}
     for artist_id in top_artists_dict.keys():
         rel_artists_resp = spotify.artist_related_artists(artist_id)
-        add_artists_to_dict(rel_artists_resp, related_artists_dict)
+        new_rel_artists = parse_artist_response(rel_artists_resp['artists'])
+        related_artists_dict.update(new_rel_artists)
 
     return related_artists_dict
 
 
 def parse_artist_response(artists_response):
-    """Takes results of API call for top artists and returns a dictionary of artists
+    """Takes results of API call for artists and returns a dictionary of artists
 
     The returned dictionary uses Spotify IDs as keys and artist name as values"""
 
     artists_dict = {}
 
     # Iterate through list of artist items in response
-    for artist in artists_response['items']:
+    for artist in artists_response:
 
         # Assigns artist's name as value to key of Spotify ID
         artist_id = artist['id']
@@ -34,17 +35,3 @@ def parse_artist_response(artists_response):
         artists_dict[artist_id] = artist_name
 
     return artists_dict
-
-
-def add_artists_to_dict(artists_response, original_dict):
-    """Adds artists from results of API call for related artists to a dictionary
-
-    The dictionary uses Spotify IDs as keys and artist name as values"""
-
-    # Iterate through list of artists in response
-    for artist in artists_response['artists']:
-
-        # Assigns artist's name as value to key of Spotify ID in given dicitonary
-        artist_id = artist['id']
-        artist_name = artist['name']
-        original_dict[artist_id] = artist_name
--- tests.py
+++ tests.py
@@ -28,30 +28,13 @@ class TestSongkick(unittest.TestCase):
 class TestAnalyzation(unittest.TestCase):
 
     def test_parse_artist_response(self):
-        sample_response = sample_apis.top_artists
+        sample_response = sample_apis.top_artists['items']
         true_result = analyzation.parse_artist_response(sample_response)
         expected_result = {'5HJ2kX5UTwN4Ns8fB5Rn1I': 'clipping.',
                            '6Tyzp9KzpiZ04DABQoedps': 'Little Dragon',
                            '0QJIPDAEDILuo8AIq3pMuU': 'M.I.A.'}
 
         self.assertEqual(true_result, expected_result)
-
-    def test_add_artists_to_dict(self):
-        true_result = {}
-        analyzation.add_artists_to_dict(sample_apis.clipping_related_1, true_result)
-
-        expected_result_first = {'6Jrxnp0JgqmeUX1veU591p': 'Santigold',
-                                 '0S05AeePINj4CeTVMfysIu': 'Rye Rye'}
-
-        self.assertEqual(true_result, expected_result_first)
-
-        analyzation.add_artists_to_dict(sample_apis.clipping_related_2, true_result)
-
-        expected_result_second = {'6Jrxnp0JgqmeUX1veU591p': 'Santigold',
-                                  '0S05AeePINj4CeTVMfysIu': 'Rye Rye',
-                                  '134GdR5tUtxJrf8cpsfpyY': 'Elliphant'}
-
-        self.assertEqual(true_result, expected_result_second)
 
 
 class TestSpotifyOauth(unittest.TestCase):
